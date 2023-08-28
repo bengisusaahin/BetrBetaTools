0e864d99d3cb1d180c1b5602a00cd8daa2b8818c
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-08-28 19:21:49 +0300

    Deleted app names, now just icons appeared

--- app/src/main/java/com/example/betrbeta/MainActivity.kt
+++ app/src/main/java/com/example/betrbeta/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardActions.Companion.Default
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -32,41 +33,46 @@ data class App(val name: String, val genre: String)
 fun GroupedApps(apps: List<App>, groupName: String) {
     Column(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(10.dp)
             .fillMaxWidth()
-            .background(Color(0,0,0,40), RoundedCornerShape(16.dp))
-            .padding(16.dp),
+            .background(Color(0, 0, 0, 40), RoundedCornerShape(16.dp))
+            .padding(10.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         LazyColumn {
-            items(apps.size) { index ->
-                val app = apps[index]
+            val chunkedApps = apps.chunked(3) // Split apps into groups of 3
+            items(chunkedApps.size) { groupIndex ->
+                val groupApps = chunkedApps[groupIndex]
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(4.dp),
+                        .padding(top = 1.dp, start = 1.dp, end = 1.dp, bottom = 1.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    // You can use your actual icons here
-                    Icon(
-                        imageVector = Icons.Default.Check,
-                        contentDescription = null,
-                        tint = Color.White,
-                        modifier = Modifier.size(32.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = app.name,
-                        color = Color.White,
-                        fontSize = 16.sp,
-                        modifier = Modifier.padding(end = 8.dp)
-                    )
+                    groupApps.forEachIndexed { index, app ->
+                        // You can use your actual icons here
+                        Icon(
+                            imageVector = Icons.Default.AccountBox,
+                            contentDescription = null,
+                            tint = Color.White,
+                            modifier = Modifier.size(64.dp)
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        if (index < groupApps.size - 1) {
+                            Spacer(modifier = Modifier.width(4.dp))
+                        }
+                    }
+                }
+                if (groupIndex < chunkedApps.size - 1) {
+                    Spacer(modifier = Modifier.height(16.dp))
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun GroupedAppsList(appsGroup1: List<App>, appsGroup2: List<App>) {
@@ -110,13 +116,19 @@ fun AppScreen() {
     val appsGroup1 = listOf(
         App("App1", ""),
         App("App2", ""),
+        App("App3", ""),
+        App("App1", ""),
+        App("App2", ""),
         App("App3", "")
     )
 
     val appsGroup2 = listOf(
         App("App4", ""),
         App("App5", ""),
-        App("App6", "")
+        App("App6", ""),
+        App("App1", ""),
+        App("App2", ""),
+        App("App3", "")
     )
 
     GroupedAppsList(appsGroup1, appsGroup2)
