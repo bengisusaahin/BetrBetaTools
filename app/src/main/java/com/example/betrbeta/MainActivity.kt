b360650fd49bd6efa2630a3188c6896ce657014e
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-08-28 13:18:46 +0300

    Grouped in 2 groups

--- app/src/main/java/com/example/betrbeta/MainActivity.kt
+++ app/src/main/java/com/example/betrbeta/MainActivity.kt
@@ -1,28 +1,74 @@
-package com.example.betrbeta
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardActions.Companion.Default
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.betrbeta.ui.theme.BetrBetaTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+data class App(val name: String, val genre: String)
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            BetrBetaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun GroupedApps(apps: List<App>, groupName: String) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .background(Color.Red)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = groupName,
+            fontSize = 20.sp,
+            color = Color.White,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+
+        LazyColumn {
+            items(apps.size) { index ->
+                val app = apps[index]
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(4.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Greeting("Android")
+                    // You can use your actual icons here
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = app.name,
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
                 }
             }
         }
@@ -30,17 +76,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BetrBetaTheme {
-        Greeting("Android")
+fun GroupedAppsList(appsGroup1: List<App>, appsGroup2: List<App>) {
+    Column {
+        GroupedApps(apps = appsGroup1, groupName = "Tools")
+        GroupedApps(apps = appsGroup2, groupName = "Games")
     }
 }
+@Preview
+@Composable
+fun AppScreen() {
+    val appsGroup1 = listOf(
+        App("App1", ""),
+        App("App2", ""),
+        App("App3", "")
+    )
+
+    val appsGroup2 = listOf(
+        App("App4", ""),
+        App("App5", ""),
+        App("App6", "")
+    )
+
+    GroupedAppsList(appsGroup1, appsGroup2)
+}
+
+@Composable
+fun App() {
+    MaterialTheme {
+        AppScreen()
+    }
+}
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+annotation class AndroidEntryPoint
